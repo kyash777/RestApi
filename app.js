e238954e491fb2fedb8768dade2cf4979dce0263
yash kshatriya <yashkshatriya.67@gmail.com>
2023-04-17 20:05:56 +0530

    second commit

--- app.js
+++ app.js
@@ -43,18 +43,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 /////////////////////////////////*code to handle cors////////////////////////////////////
-// app.use((req,res,next)=>{
-//     res.header("Access-Control-Allow-Origin","*");
-//     res.header(
-//         "Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept,Authorization"
-//     );
-//     if(req.method==="OPTIONS"){
-//         res.header("Access-Control-Allow-Methods","PUT,POST,PATCH,DELETE");
-//         return res.status(200).json({});
-//     }
-//     next();
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header(
+        "Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept,Authorization"
+    );
+    if(req.method==="OPTIONS"){
+        res.header("Access-Control-Allow-Methods","PUT,POST,PATCH,DELETE");
+        return res.status(200).json({});
+    }
+    next();
 
-// })
+})
 
 //////////////////////////////////creating authentication middleware///////////////////////
 
